4f7b1b24007988c7db65373866bbf033426ded54
jpceci <cecilia_jp@live.com>
2023-03-31 14:55:44 -0400

    Agregar archivos de proyecto.

---
+++ ProgGrafica/Game.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+namespace ProgGrafica
+{
+    public class Game: GameWindow
+    {
+        public Game(int width, int height, string title)
+            : base(width, height, GraphicsMode.Default, title)
+        {
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            GL.ClearColor(Color4.White);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.Ortho(-5, 5, -5, 5, -5, 5);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.Begin(PrimitiveType.LineLoop);
+            GL.Color3(1.0f, 0.0f, 0.0f);
+            GL.Vertex3(-1.0f, -1.0f, 0.0f);
+            GL.Color3(0.0f, 1.0f, 0.0f);
+            GL.Vertex3(1.0f, -1.0f, 0.0f);
+            GL.Color3(0.0f, 0.0f, 1.0f);
+            GL.Vertex3(0.0f, 1.0f, 0.0f);
+            GL.End();
+
+            SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+    }
+}
